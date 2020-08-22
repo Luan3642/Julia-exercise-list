39491d4ce56bbf684d77146d03b9fbdd6b7f7bfb
luan magalhães <luan3642@hotmail.com>
2020-08-22 14:22:07 -0300

    exercicios

---
+++ ex1.jl
@@ -0,0 +1,5 @@
+precoLivro = [50.4, 25.0, 60.14, 25.1, 41.6, 30.6, 80.4, 59.5, 14.0, 47.6, 98.7, 58.4];
+
+
+#1) aplique um desconto de 90 em todos os em cada livro
+descontoLivro = map(x-> x * 0.90, precoLivro)
---
+++ ex2.jl
@@ -0,0 +1,5 @@
+precoLivro = [50.4, 25.0, 60.14, 25.1, 41.6, 30.6, 80.4, 59.5, 14.0, 47.6, 98.7, 58.4];
+
+
+#2) Some todos os livros
+sum(map(x -> x, precoLivro))
