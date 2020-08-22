2fa4c6cf697c776138980e384e1ee6505a857787
luan magalhães <luan3642@hotmail.com>
2020-08-22 14:58:32 -0300

    ...

--- ex2.jl
+++ ex2.jl
@@ -1,5 +1,5 @@
 precoLivro = [50.4, 25.0, 60.14, 25.1, 41.6, 30.6, 80.4, 59.5, 14.0, 47.6, 98.7, 58.4];
 
 
-#2) Some todos os livros
-sum(map(x -> x, precoLivro))
+#2) Some todos os livros aonde o preco for maior que 50
+sum([i for i in precoLivro if i > 50])
